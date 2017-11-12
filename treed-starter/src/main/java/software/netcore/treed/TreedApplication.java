30b9a85857ccf26e5bf4303dfe5121ef7bb5445e
fifo <jozef.petrik@unimus.net>
2017-11-12 11:51:37 +0100

    Change to multi-module maven structure, add starter and data modules

---
+++ treed-starter/src/main/java/software/netcore/treed/TreedApplication.java
@@ -0,0 +1,12 @@
+package software.netcore.treed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreedApplication.class, args);
+	}
+}
---
+++ treed-starter/src/test/java/software/netcore/treed/TreedApplicationTests.java
@@ -0,0 +1,16 @@
+package software.netcore.treed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
