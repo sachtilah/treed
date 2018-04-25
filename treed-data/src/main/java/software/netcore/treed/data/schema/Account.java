8a71892ea032a27b82de63e3dbd0f455c4874f9a
fifo <jozef.petrik@unimus.net>
2018-04-25 16:17:28 +0200

    Add Piktogram data model object

--- treed-data/src/main/java/software/netcore/treed/data/schema/Account.java
+++ treed-data/src/main/java/software/netcore/treed/data/schema/Account.java
@@ -11,7 +11,6 @@ import java.sql.Timestamp;
  */
 @Getter
 @Setter
-@NoArgsConstructor
 @Table(name = "account")
 @Entity
     public class Account extends AbstractEntity {
---
+++ treed-data/src/main/java/software/netcore/treed/data/schema/Piktogram.java
@@ -0,0 +1,25 @@
+package software.netcore.treed.data.schema;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+/**
+ * @since v.1.7.0
+ */
+@Getter
+@Setter
+@Table(name = "piktogram")
+public class Piktogram extends AbstractEntity {
+
+    @Lob
+    @Column(name = "bytes", nullable = false)
+    private byte[] bytes;
+
+    @Column(name = "term", nullable = false)
+    private String term;
+
+}
